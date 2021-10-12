86643e5b487b7d78e59221867decd8424b5ddee3
milos_pr <milos_pr@nextgenio-login1.localdomain>
2021-10-12 13:23:14 +0100

    zero_stats

--- plot/zero_stats.py
+++ plot/zero_stats.py
@@ -6,12 +6,12 @@ import subprocess
 counter=0
 zero_sols=0
 
-for filename in glob.glob(os.path.join(sys.argv[1],"*")):
+for filename in glob.glob(os.path.join(sys.argv[1],"statsfile*")):
     with open(filename) as f:
         line = subprocess.check_output(['tail', '-1', filename])[:-1].decode("utf-8")
         splitted=line.split(' ')
         opt_energy=splitted[1]
-        print(opt_energy)
+        #print(opt_energy)
         if float(opt_energy) == 0:
             zero_sols+=1
         counter+=1
